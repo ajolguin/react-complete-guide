ae34fd3ac5ea805e10155c372b4b14b772164685
Adrian <aolguin8097@gmail.com>
2022-01-19 10:36:22 -0800

    Rendering lists of data dynamically

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -1,9 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "./ExpensesFilter.css";
 
 const ExpensesFilter = (props) => {
-  const [selectedYear, setSelectedYear] = useState("");
   const dropdownChangeHandler = (event) => {
     props.onChangeFilter(event.target.value);
     // console.log(event.target.value);
